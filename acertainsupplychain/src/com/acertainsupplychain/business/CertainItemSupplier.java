ef39108f6acc81e88af7fe99b741871df83cd638
Philip Munksgaard <pmunksgaard@gmail.com>
2014-01-16 15:28:06 +0100

    Cleanup imports

--- acertainsupplychain/src/com/acertainsupplychain/business/CertainItemSupplier.java
+++ acertainsupplychain/src/com/acertainsupplychain/business/CertainItemSupplier.java
@@ -7,9 +7,6 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
