2af7bee4d4e28835087449a77f8ddfd4c6495f5f
Philip Munksgaard <pmunksgaard@gmail.com>
2014-01-16 02:27:02 +0100

    Add SupplyChainConstants

---
+++ acertainsupplychain/src/com/acertainsupplychain/utils/SupplyChainConstants.java
@@ -0,0 +1,12 @@
+package com.acertainsupplychain.utils;
+
+/**
+ * BookStoreConstants declares the constants used in the CertainBookStore (by
+ * both servers and clients)
+ *
+ */
+public final class SupplyChainConstants {
+
+    public static final String PROPERTIES_PATH = "src/server.properties";
+
+}
