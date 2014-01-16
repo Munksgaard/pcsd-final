ebc7a18c8b953c47131e8479dfba62ca6e9a3c04
Philip Munksgaard <pmunksgaard@gmail.com>
2014-01-16 02:27:37 +0100

    Add Logger and LogException

---
+++ acertainsupplychain/src/com/acertainsupplychain/utils/LogException.java
@@ -0,0 +1,19 @@
+package com.acertainsupplychain.utils;
+
+/**
+ * This exception is used by the Logger class
+ */
+@SuppressWarnings("serial")
+public class LogException extends Exception {
+
+    private final Exception e;
+
+    public LogException(Exception e) {
+        this.e = e;
+    }
+
+    public Exception getException() {
+        return e;
+    }
+
+}
---
+++ acertainsupplychain/src/com/acertainsupplychain/utils/Logger.java
@@ -0,0 +1,67 @@
+package com.acertainsupplychain.utils;
+
+import java.util.Properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+/**
+ * The logger class is used to log actions from the OrderManager and ItemSuppliers.
+ */
+public class Logger {
+
+    private final BufferedWriter out;
+
+    public Logger(String filename) throws LogException {
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream(SupplyChainConstants.PROPERTIES_PATH));
+        } catch (Exception e) {
+            throw new LogException(e);
+
+        }
+
+        String dir = props.getProperty(SupplyChainConstants.LOG_DIR);
+
+        // Create log directory if it doesn't already exist.
+        File file = new File(dir);
+        file.mkdir();
+
+        // Purge the log file
+        file = new File(dir + File.separator + filename);
+        if (file.exists()) {
+            if (file.isFile()) {
+                file.delete();
+            } else {
+                throw new LogException(new Exception("Log file is a directory!"));
+            }
+        }
+
+        // Open the writer
+        try {
+            FileWriter fstream = new FileWriter(file, true);
+            this.out = new BufferedWriter(fstream);
+        } catch (Exception e) {
+            throw new LogException(e);
+        }
+    }
+
+    public void log(Object obj) throws LogException {
+        try {
+            out.write(SupplyChainUtility.serializeObject(obj) + "\n");
+            out.flush();
+        } catch (Exception e) {
+            throw new LogException(e);
+        }
+    }
+
+    public void close() throws LogException {
+        try {
+            out.close();
+        } catch (Exception e) {
+            throw new LogException(e);
+        }
+    }
+}
