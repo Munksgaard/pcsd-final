ea9b7b46c0576d330333dd865b4b40f1a8df9ebc
Philip Munksgaard <pmunksgaard@gmail.com>
2014-01-16 17:45:11 +0100

    Add extractRequest in utilities

--- acertainsupplychain/src/com/acertainsupplychain/utils/SupplyChainUtility.java
+++ acertainsupplychain/src/com/acertainsupplychain/utils/SupplyChainUtility.java
@@ -1,9 +1,30 @@
 package com.acertainsupplychain.utils;
 
+import java.io.IOException;
+import java.io.Reader;
+
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.StaxDriver;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class SupplyChainUtility {
+
+    public static Object extractRequest(HttpServletRequest request)
+        throws IOException {
+
+        // Extract XML
+        Reader reader = request.getReader();
+        int len = request.getContentLength();
+        char buf[] = new char[len];
+        reader.read(buf);
+        reader.close();
+
+        // Convert XML to object
+        String str = new String(buf);
+        XStream xmlStream = new XStream(new StaxDriver());
+        return xmlStream.fromXML(str);
+    }
 
     /**
      * Convert object to XML
